f2007b0ced3b9a065c943290045fd6a870273d4c
huangkeke28 <huangkeup@163.com>
2020-09-28 20:37:24 +0800

    练习1

--- chess-sys/src/main/java/test/TestWebSocket.java
+++ chess-sys/src/main/java/test/TestWebSocket.java
@@ -3,6 +3,7 @@ package test;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 @ServerEndpoint(value="/webSocketTest/{userId}")
 public class TestWebSocket {
@@ -14,11 +15,30 @@ public class TestWebSocket {
         System.out.println("建立连接" + userIdStr);
         this.userId = Integer.parseInt(userIdStr);
 
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    String resp = "" + System.currentTimeMillis();
+                    try {
+                        session.getBasicRemote().sendText(resp);
+                        Thread.sleep(1000);
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
     }
 
     @OnMessage
-    public void onMessage(String message, Session session) {
+    public void onMessage(String message, Session session) throws IOException {
         System.out.println("收到消息！Message=" + message);
+        //服务器给客户端返回一个消息
+        //返回一个当前的时间戳
+        String resp = "" + System.currentTimeMillis();
+        session.getBasicRemote().sendText(resp);
     }
 
     @OnError
